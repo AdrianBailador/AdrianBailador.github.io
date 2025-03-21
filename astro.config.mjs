764a6df926082e46e625de2705db7423f5339fdc
Adrian Bailador Panero <adrianbailador@hotmail.com>
2025-03-21 17:28:25 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import solidJs from "@astrojs/solid-js"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://AdrianBailador.github.io",
+  site: 'https://AdrianBailador.github.io',
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
 })
