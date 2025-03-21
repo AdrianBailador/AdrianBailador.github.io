a67784ad7c8e239de8a30373f9c8147e934c0af5
Adrian Bailador <adrianbailador@hotmail.com>
2025-03-21 17:59:54 +0000

    Update social media

--- src/consts.ts
+++ src/consts.ts
@@ -56,26 +56,26 @@ export const SOCIALS: Socials = [
   { 
     NAME: "Email",
     ICON: "email", 
-    TEXT: "markhorn.dev@gmail.com",
+    TEXT: "abailador.dev@gmail.com",
     HREF: "mailto:markhorn.dev@gmail.com",
   },
   { 
     NAME: "Github",
     ICON: "github",
-    TEXT: "markhorn-dev",
-    HREF: "https://github.com/markhorn-dev/astro-sphere"
+    TEXT: "adrianbailador-dev",
+    HREF: "https://github.com/AdrianBailador"
   },
   { 
     NAME: "LinkedIn",
     ICON: "linkedin",
-    TEXT: "markhorn-dev",
-    HREF: "https://www.linkedin.com/in/markhorn-dev/",
+    TEXT: "adrianbailador-dev",
+    HREF: "https://linkedin.com/in/adrianbailadorpanero",
   },
   { 
     NAME: "Twitter",
     ICON: "twitter-x",
-    TEXT: "markhorn_dev",
-    HREF: "https://twitter.com/markhorn_dev",
+    TEXT: "adrianbailador-dev",
+    HREF: "https://medium.com/@adrianbailador",
   },
 ]
 
