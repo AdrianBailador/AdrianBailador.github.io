e91bc8ff6340c0fe936c5d2db3dd7e7a4902f772
Adrian Bailador <adrianbailador@hotmail.com>
2025-03-22 14:59:57 +0000

    change title

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@ import type { Site, Page, Links, Socials } from "@types"
 
 // Global
 export const SITE: Site = {
-  TITLE: ".NET | C# | Azure | JS",
+  TITLE: "drian Bailador | .NET ",
   DESCRIPTION: "Welcome to my portfolio and blog about software engineering, coding and technology. Talk about .NET, C#, Azure, Visual Studio, and a bit of Next.js.",
   AUTHOR: "Adrian Bailador Panero",
 }
